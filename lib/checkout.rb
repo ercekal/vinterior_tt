81d2df73d6cfbd5dff286a2a319ff7befb8e37b7
ercekal <ercekalabalikoglu@gmail.com>
2017-05-19 12:22:45 +0100

    adds the first promotion which is >60

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,15 +2,27 @@ require_relative 'item'
 
 class Checkout
 
+
 attr_reader :total, :basket
 
-  def initialize
+  def initialize(promotional_rules)
+    @promotional_rules = promotional_rules
     @basket = []
     @total = 0
   end
 
   def scan(item)
+    @basket << item
     @total += item.price
+    make_discount
   end
 
+  def make_discount
+    if @promotional_rules == 1
+      if @total > 60
+        @total = @total * 0.9
+      end
+    end
+    @total
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,10 +7,26 @@ describe Checkout do
 
   describe '#scan' do
     it 'should add correct item price to total' do
-      co = Checkout.new
+      co = Checkout.new(0)
       chair = Item.new 001,  "chair",  7.0
       co.scan(chair)
       expect(co.total).to eq 7.0
+    end
+
+    it 'should add the item to basket' do
+      co = Checkout.new(0)
+      chair = Item.new 001,  "chair",  7.0
+      co.scan(chair)
+      expect(co.basket).to include chair
+    end
+
+    it 'should make promotion if total is over 60' do
+      co = Checkout.new(1)
+      chair = Item.new 001,  "chair",  30.0
+      table = Item.new 002,  "table",  40.0
+      co.scan(chair)
+      co.scan(table)
+      expect(co.total).to eq 63
     end
   end
 end
